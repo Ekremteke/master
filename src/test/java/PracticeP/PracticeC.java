2bd533c9e63f87cdae276cbb9767be903b74a80c
ihsankurt17 <102886451+ihsankurt17@users.noreply.github.com>
2022-04-14 20:01:28 +0200

    ihsan

--- src/test/java/PracticeP/PracticeC.java
+++ src/test/java/PracticeP/PracticeC.java
@@ -7,6 +7,7 @@ public class PracticeC {
         System.out.println("herhangi birsey");
         System.out.println("baska birsey");
         System.out.println("ihsan dan degisiklik");
-        
+
+        System.out.println("ihsan dan degisiklik");
     }
 }
